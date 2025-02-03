8d4fba6d079aca8fabb127c8d2cd454aa8dbe25b
Eric Lin <ericshenl2789@nycstudents.net>
2025-02-03 13:52:29 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,18 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import U7T1.Car;
 
 public class Main{
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("Ben");
-        names.add("Andy");
-        names.add("David");
-        String[] moreNames = {"Crystal", "Monica", "Toby", "Nancy"};
-        int namesAmt = names.size();
-        int moreNamesAmt = moreNames.length;
-        int thirdNameLength = names.get(2).length();
-        int secondNameLength = moreNames[1].length();
-        int lengths = namesAmt + moreNamesAmt + thirdNameLength + secondNameLength;
-        System.out.println(lengths); // should print 18
+
+        // create an array with the known values
+        Car[] carArray = {new Car("Bugatti", 212), new Car("Porche", 454), new Car("Challenger", 123)};
+
+        // use the Arrays.asList utility method to convert the array to a list "inline"
+        ArrayList<Car> cArrayList = new ArrayList<Car>(Arrays.asList(carArray));
+        System.out.println(cArrayList);
 
     }
 }
